c16b09fbe930c13a773f00e7b865132ad25687dd
Chandrachud Siddharth <chandrachudsiddharth@gmail.com>
2026-08-21 22:51:43 +0530

    test: add liquidation edge-case regressions

--- contracts/test/LendingPoolSafety.t.sol
+++ contracts/test/LendingPoolSafety.t.sol
@@ -10,42 +10,32 @@ contract LendingPoolSafetyTest is KarmaFixture {
     function test_defundCannotWithdrawOutstandingPrincipal() public {
         fundCollateral(alice, TEN_ETH);
         attest(alice, 900);
-
-        vm.prank(alice);
-        pool.borrow(10_000e6);
-
+        vm.prank(alice); pool.borrow(10_000e6);
         uint256 balance = usdc.balanceOf(address(pool));
         uint256 protectedPrincipal = 10_000e6;
-
-        vm.prank(owner);
-        vm.expectRevert(LendingPool.InsufficientReserves.selector);
-        pool.defund(balance - protectedPrincipal + 1);
-
-        vm.prank(owner);
-        pool.defund(balance - protectedPrincipal);
-
+        vm.prank(owner); vm.expectRevert(LendingPool.InsufficientReserves.selector); pool.defund(balance - protectedPrincipal + 1);
+        vm.prank(owner); pool.defund(balance - protectedPrincipal);
         assertEq(usdc.balanceOf(address(pool)), protectedPrincipal);
     }
 
     function test_defundStillWorksWhenNoDebt() public {
         uint256 balance = usdc.balanceOf(address(pool));
-        uint256 withdrawal = balance / 2;
-
-        vm.prank(owner);
-        pool.defund(withdrawal);
-
-        assertEq(usdc.balanceOf(address(pool)), balance - withdrawal);
+        vm.prank(owner); pool.defund(balance / 2);
+        assertEq(usdc.balanceOf(address(pool)), balance / 2);
     }
 
     function test_zeroOraclePriceIsRejected() public {
-        fundCollateral(alice, TEN_ETH);
-        attest(alice, 900);
+        fundCollateral(alice, TEN_ETH); attest(alice, 900);
+        vm.prank(owner); priceOracle.setPrice(address(weth), 0);
+        vm.prank(alice); vm.expectRevert(LendingPool.InvalidPrice.selector); pool.borrow(1_000e6);
+    }
 
-        vm.prank(owner);
-        priceOracle.setPrice(address(weth), 0);
-
-        vm.prank(alice);
-        vm.expectRevert(LendingPool.InvalidPrice.selector);
-        pool.borrow(1_000e6);
+    function test_tinyDebtCanBeLiquidated() public {
+        fundCollateral(alice, TEN_ETH); attest(alice, 900);
+        vm.prank(alice); pool.borrow(1);
+        vm.prank(owner); priceOracle.setPrice(address(weth), 1e8);
+        usdc.mint(liquidator, 1); vm.prank(liquidator); usdc.approve(address(pool), 1);
+        vm.prank(liquidator); pool.liquidate(alice, 1);
+        assertEq(pool.debtOf(alice), 0);
     }
 }
